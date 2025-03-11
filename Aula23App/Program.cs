e615d7b6225b364ff905322695f5ecf3f184010b
Vitor Augusto <vitor.acb@hotmail.com>
2025-03-11 15:38:28 -0300

    Prática aula 23
    Pratica de entrada de dados 2

---
+++ Aula23App/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Globalization; // Biblioteca para utilizar o CultureInfo
+
+
+    int n1 = int.Parse(Console.ReadLine());       // Converte a leitura de string do Console.Readline para um inteiro com o comando .Parse
+    char ch = char.Parse(Console.ReadLine());     // Converte a leitura de string do Console.Readline para um char com o comando .Parse
+    double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); // CultureInfo.InvariantCulture serve para que o programa aceite o ponto como separador de casas decimais
+
+    string[] vet = Console.ReadLine().Split(' ');
+    string nome = vet [0];
+    char sexo = char.Parse(vet[1]);
+    int idade = int.Parse(vet[2]);
+    double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+    Console.WriteLine("Você digitou:");
+    Console.WriteLine(n1);
+    Console.WriteLine(ch);
+    Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture)); // CultureInfo.InvariantCulture serve para que o programa aceite o ponto como separador de casas decimais
+    Console.WriteLine(nome);
+    Console.WriteLine(sexo);
+    Console.WriteLine(idade);
+    Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));

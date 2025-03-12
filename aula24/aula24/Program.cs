8c04cb9b06ebfccd73819d424213f787fe859b38
Vitor Augusto <vitor.acb@hotmail.com>
2025-03-12 12:45:19 -0300

    Estudos

---
+++ aula24/aula24/Program.cs
@@ -0,0 +1,19 @@
+﻿//Exercicio1) 
+// Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos.
+
+
+using System.Globalization;
+
+Console.WriteLine("Digite um numero inteiro: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero inteiro: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("SOMA = " + (a + b));
+
+
+
+
+
---
+++ aula24/exercicio2/exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Exercicio2)
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//casas decimais conforme exemplos.
+//Fórmula da área: area = π . raio2
+//Considere o valor de π = 3.14159
+
+using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+
+double pi = 3.14159, raio, area;
+
+Console.WriteLine("Digite o valor do raio: ");
+raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+area = pi * (raio * 2);
+
+System.Console.WriteLine("O valor da area é de: " + area.ToString("F4"));

76981f56ae52edf4060b58c60a11b2898d6a94b9
Vitor Augusto <vitor.acb@hotmail.com>
2025-03-27 12:33:07 -0300

    Exercício de Intervalos

---
+++ Comparadores/comparadoresaula/exerciciointervalo/exerciciointervalo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+
+System.Console.WriteLine("Digite um valor: ");
+double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+if (valor < 0 || valor > 100){
+    System.Console.WriteLine("Fora do intervalo de valores!");
+}
+
+else if (valor >= 0 && valor <= 25){
+    System.Console.WriteLine("Intervalo entre 0 e 25");
+}
+
+else if (valor >= 25 && valor <= 50){
+    System.Console.WriteLine("Intervalo entre 25 e 50");
+}
+
+else if (valor >= 50 && valor <= 75){
+    System.Console.WriteLine("Intervalo entre 50 e 75");
+}
+else if (valor >= 75 && valor <= 100){
+    System.Console.WriteLine("Intervalo entre 75 e 100");
+}
--- aula23exercicio/AULA23EXE/Program.cs
+++ aula23exercicio/AULA23EXE/Program.cs
@@ -1,25 +1,10 @@
-﻿using System.Globalization;
-using System;
+﻿using System;
 
-Console.WriteLine("Digite seu nome completo:");
-string nome = Console.ReadLine(); 
-
-Console.WriteLine("Quantos quartos tem na sua casa?");
-int quartos = int.Parse(Console.ReadLine());
-
-System.Console.WriteLine("Entre com o preço do produto: "); 
-double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-Console.WriteLine("Entre seu ultimo nome, idade e altura: ");
-string[] vet = Console.ReadLine().Split(' ');
-string nome1 = vet [0];
-int quartos1 = int.Parse(vet[1]);
-double preco1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+int valor;
+System.Console.WriteLine("Digite um valor: ");
+valor = int.Parse(Console.ReadLine());
 
 
-System.Console.WriteLine(nome);
-System.Console.WriteLine(quartos);
-System.Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
-System.Console.WriteLine(nome1);
-System.Console.WriteLine(quartos1);
-System.Console.WriteLine(preco1.ToString("F2", CultureInfo.InvariantCulture));
+if (valor < 0 || valor > 100){
+    System.Console.WriteLine("Fora do intervalo de valores!");
+}

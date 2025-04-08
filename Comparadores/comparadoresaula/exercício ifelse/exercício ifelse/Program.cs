a321eef022c032bdddd027ee9e7551003ac5544a
Vitor Augusto <vitor.acb@hotmail.com>
2025-04-08 13:14:10 -0300

    Exercicio imposto de renda
    Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
    que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
    qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.

---
+++ Comparadores/comparadoresaula/exercício ifelse/exercício ifelse/Program.cs
@@ -0,0 +1,27 @@
+﻿double salario, imposto;
+
+System.Console.WriteLine("Digite o valor dos eu salario: ");
+salario = double.Parse(Console.ReadLine());
+
+if (salario <= 2000.00){
+    System.Console.WriteLine("Isento");
+}
+
+else if (salario <= 3000.00){
+      imposto = (salario - 2000.00) * 0.08;
+
+    System.Console.WriteLine("Seu Imposto de renda é: " + imposto.ToString("F2"));
+
+}
+
+else if (salario <= 4500.00){
+    imposto = (salario - 3000) * 0.18 + 1000 * 0.08;
+    System.Console.WriteLine("Seu Imposto de renda é: " + imposto.ToString("F2"));
+}
+
+else {
+
+    imposto = (salario - 4500) * 0.28 + 1500 * 0.18 + 1000 * 0.08;
+    System.Console.WriteLine("Seu Imposto de renda é: " + imposto.ToString("F2"));
+
+}

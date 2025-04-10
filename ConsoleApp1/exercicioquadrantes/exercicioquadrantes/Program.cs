f8b1436f3cddeeedcba8a4fdf9d20ab2caa8bd30
Vitor Augusto <vitor.acb@hotmail.com>
2025-04-10 15:16:17 -0300

    jogohoras
    exercício para calcular as horas de um jogo

---
+++ ConsoleApp1/exercicioquadrantes/exercicioquadrantes/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+
+int horaInicial = int.Parse(valores[0]);
+int horaFinal = int.Parse (valores[1]);
+
+int duracao;
+
+if (horaInicial < horaFinal){
+    duracao = horaFinal - horaInicial;
+
+}
+
+else {
+    duracao = 24 - horaInicial + horaFinal;
+}
+
+
+
+System.Console.WriteLine(" O jogo durou " + duracao +  " HORA(S) ");

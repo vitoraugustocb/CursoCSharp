68a754b6181837c38219b989a45ef26f868a4888
Vitor Augusto <vitor.acb@hotmail.com>
2025-07-14 12:45:53 -0300

    teste
    teste

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+/* para imprimir apenas 2 casas dentro de uma variável INT eu escrevo Console.WriteLine(saldo.ToString("F2"));
+nota para lembrar depois! */
 
+double saldo = 10.239384398;
+Console.WriteLine(saldo);
+Console.WriteLine(saldo.ToString("F2")); // Formata o número para 2 casas decimais 
 
-string frase = Console.ReadLine();
-
-string x = Console.ReadLine();
-string y = Console.ReadLine();
-string z = Console.ReadLine();
-
-string s = Console.ReadLine();
-
-string[] v = s.Split(' '); // Apagar a variável s = Console.ReadLine(); e simplificar para uma váriavel Console.ReadLine().Split(' ');
-string a = v[0];   
-string b = v[1];
-string c = v[2];
-
-System.Console.WriteLine("Você digitou: " + frase);
-System.Console.WriteLine(x);
-System.Console.WriteLine(y);   
-System.Console.WriteLine(z);
-System.Console.WriteLine(a);
-System.Console.WriteLine(b);
-System.Console.WriteLine(c);
-
-string[] vet = s.Split(' ');  // Split é um método que separa uma string em várias partes, de acordo com um delimitador
-string p1 = vet[0];
-string p2 = vet[1];
-string p3 = vet[2];

14a7a0148f106ec604e1a440c32ef82f14af2efd
Vitor Augusto <vitor.acb@hotmail.com>
2025-08-11 13:06:13 -0300

    ex1 for
    Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
    X, se for o caso.

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um numero: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você digitou o numero: {0}, seus numeros impares são: ", numero);
+for (int i = 1; i <= numero; i++)
+{
+    
+    if (i % 2 != 0)
+    {
+       
+        Console.WriteLine(i);
+    }
+}
